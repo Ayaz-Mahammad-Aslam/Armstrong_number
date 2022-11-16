22a14482e53e18d190beab1c29e014134cca89e0
Ayaz Mahammad Aslam <116188331+Ayaz-Mahammad-Aslam@users.noreply.github.com>
2022-11-16 19:41:41 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,23 @@
+/* Write a program to check armstrong number in c*/
+#include<stdio.h>
+int main()
+/* An armstrong number is a number which is equal to the sum of the cubes of its individual digits.
+For example, 153 is an armstrong number as : 
+153 = (1)3+ (5)3 + (3)3 153, = 1 + 125 + 27 153 = 153 */
+{
+    int n,r,sum=0,temp;
+    printf("Enter a number :\n");
+    scanf("%d",&n);
+    temp = n;
+    while(n>0)
+    {
+        r = n%10;
+        sum = sum + (r*r*r);
+        n = n/10;
+    }
+    if(temp == sum)
+    printf("Armstrong number");
+    else
+    printf("Not Armstrong");
+    return 0;
+}
